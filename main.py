0855b79b7b1df0c7658f129c20a39e7ff9aa8fa3
ngAtesz <attila.molnar@codecool.com>
2017-06-28 23:29:33 +0200

    Databse schema creation script added.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import data_manager
+import init_db
 
 
 def main():
-    conn_data = data_manager.get_connection_data('postgres')
-    with data_manager.establish_connection(connection_data=conn_data) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT 'Hello Codecool'")
-            result = cursor.fetchall()
-            print(result)
+    init_db.init_db()
+    init_db.create_schema()
 
 if __name__ == '__main__':
     main()
